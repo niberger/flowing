c6bc92096182d565e9934ab161e9c9bb1f59d249
Nicolas Berger <contact@nberger.ch>
2017-09-03 17:29:55 +0200

    subscribe

--- flow.cs
+++ flow.cs
@@ -21,8 +21,8 @@ namespace Flowing
     }
     public static class Flow
     {
-        public static IFlow<T> Value<T>(T value) => new Flow<T>(Observable.Return(new Value<T>(value)));
-        public static IFlow<T> Error<T>(String error) => new Flow<T>(Observable.Return(new Error<T>(error)));
+        public static IFlow<T> Return<T>(T value) => new Flow<T>(Observable.Return(new Value<T>(value)));
+        public static IFlow<T> Error<T>(Exception error) => new Flow<T>(Observable.Return(new Error<T>(error)));
         public static IFlow<T> Pending<T>() => new Flow<T>(Observable.Return(new Pending<T>()));
         public static IFlow<T> Flatten<T>(this IFlowState<IFlow<T>> state)
         {
@@ -31,7 +31,7 @@ namespace Flowing
                 case Value<IFlow<T>> val:
                     return val.Val;
                 case Error<IFlow<T>> err:
-                    return Error<T>(err.Msg);
+                    return Error<T>(err.Ex);
                 default:
                     return Pending<T>();
             }
@@ -45,6 +45,21 @@ namespace Flowing
             return source.Select(x => collectionSelector(x)).Select(c => c.Select(resultSelector))
         }// */
         public static IFlow<S> Select<T,S>(this IFlow<T> flow, Func<T,S> selector) => new Flow<S>(flow.StateObs.Select(t => t.Select(selector)));
-        public static IFlow<T> Flatten<T>(this IFlow<IFlow<T>> flow) => new Flow<T>(flow.StateObs.Select(state => state.Flatten().StateObs).Switch()); 
+        public static IFlow<T> Flatten<T>(this IFlow<IFlow<T>> flow) => new Flow<T>(flow.StateObs.Select(state => state.Flatten().StateObs).Switch());
+        public static void Subscribe<T>(this IFlow<T> flow, Action<T> onNext, Action<Exception> onError, Action onPending)
+        {
+            Action<IFlowState<T>> stateObsOnNext = s => {
+                if(s is Value<T> val)
+                    onNext(val.Val);
+                else if(s is Error<T> err)
+                    onError(err.Ex);
+                else if(s is Pending<T>)
+                    onPending();
+            };
+
+            flow.StateObs.Subscribe(stateObsOnNext);
+        }
+        public static void Subscribe<T>(this IFlow<T> flow, Action<T> onNext, Action<Exception> onError) => flow.Subscribe(onNext, onError, () => {});
+        public static void Subscribe<T>(this IFlow<T> flow, Action<T> onNext) => flow.Subscribe(onNext, (ex) => {throw ex;}, () => {});
     }
 }
